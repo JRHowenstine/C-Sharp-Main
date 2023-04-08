880a97e434ddd35db9d4184aa1027dbf309a3c75
JRHowenstine <justin.howenstine@gmail.com>
2023-04-08 15:32:08 -0700

    String Assignment

---
+++ C#/StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = "This is part one, ";
+            string two = "tis be part two, ";
+            string three = "and this ends it.";
+
+            string concatenation = one + two + three;
+            Console.WriteLine(concatenation);
+
+            concatenation = concatenation.ToUpper();  // makes string all uppercase
+
+
+            Console.WriteLine(concatenation);
+
+
+
+            StringBuilder sb = new StringBuilder();  // creates a dynamic string variable
+
+            sb.AppendLine("My name is Justin.");
+            sb.AppendLine("I live in Gresham, Oregon.");
+            sb.AppendLine("I am a student of The Tech Academy.");
+            sb.AppendLine("I am studying to be a software developer.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Jesse";
+            //string quote = "The man said, \"Hello\", Jesse. \nHello on a new line. \n \tHello on a tab.";
+            //string fileName = "C:\\Users\\Owner";  // or you can put @ first and escape sequences arent needed
+            //string fileName1 = @"C:\Users\Owner";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToLower();  // makes all lower can do .ToUpper() for all Capitals
+
+
+            //Console.WriteLine(trueOrFalse);
+            //Console.ReadLine();
+
+
+            //string name = "Jesse";
+            //name = "Joe";  // creates a new string in memory doesn't just change it
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+
+            StringBuilder sb = new StringBuilder();  // creates a dynamic string variable
+
+            sb.Append("My name is Justin");  // allows you to change the string
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
