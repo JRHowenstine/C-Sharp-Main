311854106b2b34e4fa010db1f26fb4f6c77c0a8a
JRHowenstine <justin.howenstine@gmail.com>
2023-04-08 13:42:55 -0700

    Edited integer inputs

--- C#/CarInsuranceApproval/CarInsuranceApproval/Program.cs
+++ C#/CarInsuranceApproval/CarInsuranceApproval/Program.cs
@@ -12,14 +12,12 @@ namespace CarInsuranceApproval
         {
             Console.WriteLine("Car Insurance Approval Form"); 
             Console.WriteLine("What is your age?"); 
-            string userAge = Console.ReadLine();  // Get age from user and store as a string
-            int Age = Convert.ToInt32(userAge);  // Convert input to a integer variable
+            int Age = Convert.ToInt32(Console.ReadLine());  // Get age from user andConvert input to a integer variable
             Console.WriteLine("Have you ever had a DUI? Please answer \"true\" or \"false.\""); 
             string userDUI = Console.ReadLine();  // Get answer from user and store as a string
             bool DUI = Convert.ToBoolean(userDUI);  // Convert input to a boolean variable
             Console.WriteLine("How many speeding tickets do you have?");
-            string userTickets = Console.ReadLine();  // Get input from user and store as a string
-            int Tickets = Convert.ToInt32(userTickets);  // Convert input to a integer variable
+            int Tickets = Convert.ToInt32(Console.ReadLine());  // Get input from user and Convert input to a integer variable
             Console.WriteLine("Qualified?");
             if ((Age > 15) && (DUI == false) && (Tickets < 4)) // Check to see in user inputs qualify them for insurance
                 Console.WriteLine("True");  // Print true if qualified
---
+++ C#/Practice/Practice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 17;
+            int num2 = 8;
+
+            string result = num1 < num2 ? "num1 is greater than num2" : "num2 is greater than num1";
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
