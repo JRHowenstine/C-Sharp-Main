eab9efe3b125a48ee4acd4aaac230daad1722334
JRHowenstine <justin.howenstine@gmail.com>
2023-04-08 14:57:07 -0700

    Do While Loop Submission Assignment

---
+++ C#/LoopSubAssign/LoopSubAssign/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopSubAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Making a basic while loop
+            int x = 0;
+            while (x < 5)  //  Continue loop until x = 5
+            {
+                Console.WriteLine(x);
+                x++;  // Print value of x then add 1 and loop
+            }
+
+
+
+            //  Making a 'do while' statement
+            Console.WriteLine("Please guess a integer from 1 to 10.");  // User inputs guess
+            int number = Convert.ToInt32(Console.ReadLine());  //  store guess ad an integer
+            bool usersGuess = number == 6;  // define usersGuess to be false if 'number' is not 6
+
+            do  // Set the do statement
+            {
+                switch (number)  // set switch to check for specific case and a default response
+                {
+                    case 6:
+                        Console.WriteLine("You guessed the number 6. That is correct!.");
+                        usersGuess = true;  // Set the userGuess boolean to true once they guess correct value to enable leaving loop
+                        break;
+                    default: // set response for case not being met
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Next Guess:");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!usersGuess); // run the switch statement in a loop til usersGuess evaluates as true
+
+            Console.ReadLine();
+        }
+    }
+}
--- C#/Practice/Practice/Program.cs
+++ C#/Practice/Practice/Program.cs
@@ -10,11 +10,45 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            int num1 = 17;
-            int num2 = 8;
+            //int num1 = 17;
+            //int num2 = 8;
 
-            string result = num1 < num2 ? "num1 is greater than num2" : "num2 is greater than num1";
-            Console.WriteLine(result);
+            //string result = num1 < num2 ? "num1 is greater than num2" : "num2 is greater than num1";
+            //Console.WriteLine(result);
+
+
+            Console.WriteLine("What number day of the week is it?");
+            int day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("You did not enter a number 1 to 7.");
+                    break;
+
+            }
+
+
             Console.ReadLine();
         }
     }
