ef77722d40f672d4adbd03d40b86f52619910897
JRHowenstine <justin.howenstine@gmail.com>
2023-04-15 16:26:25 -0700

    Main Method Submission Assignment

---
+++ C#/MainMethodSubAssign/MainMethodSubAssign/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");  //  Have user enter intial number
+            int userNum1 = Convert.ToInt32(Console.ReadLine());  //  Store initial input as integer
+
+            mathOps math = new mathOps();  //  Instantiate mathOps class
+
+            int answer1 = math.doMath(userNum1);  //  Call mathOps Method with integer argument
+            Console.WriteLine("Your number add one equals: {0}\nPlease enter a number: ", answer1);
+
+            decimal userNum2 = Convert.ToDecimal(Console.ReadLine()); // Get new number store as decimal
+            int answer2 = math.doMath(userNum2);  //  Call mathOps Method with decimal argument
+            Console.WriteLine("Your number multiplyed by 2 equals: {0}\nPlease enter a number: ", answer2);
+
+            string userNum3 = Console.ReadLine();  //  Get new number store as string
+            int answer3 = math.doMath(userNum3);//  Call mathOps Method with string argument
+            Console.WriteLine("Subtracting 3 from your number equals: {0}", answer3);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/MainMethodSubAssign/MainMethodSubAssign/mathOps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubAssign
+{
+    class mathOps
+    {
+        public int doMath(int num1)  //  Method used if integer argument
+        {
+            int answer = num1 + 1;
+            return answer;
+        }
+
+        public int doMath(decimal num1)  //  Method used if decimal argument
+        {
+            int answer = Convert.ToInt32(num1) * 2;
+            return answer;
+        }
+
+        public int doMath(string num1)  //  Method used if string argument
+        {
+            int answer = Convert.ToInt32(num1) - 3;
+            return answer;
+        }
+    }
+}
