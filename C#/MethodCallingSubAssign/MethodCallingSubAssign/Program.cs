b416433aa53018f008415e0adb55e5386eee146e
JRHowenstine <justin.howenstine@gmail.com>
2023-04-15 15:54:57 -0700

    Calling Methods Submission Assignment

---
+++ C#/MethodCallingSubAssign/MethodCallingSubAssign/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodCallingSubAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");  //  Ask user to input a number
+
+            int userNum = Convert.ToInt32(Console.ReadLine());  //  Take user inut and save as an integer
+            mathOps math = new mathOps();  //  Instantiate the new class of math operations
+            
+            int answer1 = math.doMath1(userNum);  //  Take user input and call 1st method
+            Console.WriteLine("Your number plus 1 equals: {0}", answer1);  //  Return answer from method
+                        
+            int answer2 = math.doMath2(userNum);  //  Take user input and call 2nd method
+            Console.WriteLine("Your number multiplyed by 2 equals: {0}", answer2);  //  Return answer from method
+
+            int answer3 = math.doMath3(userNum);  //  Take user input and call 3rd method
+            Console.WriteLine("Subtracting 3 from your number equals: {0}", answer3);  //  Return answer from method
+
+            Console.ReadLine();  //  Keep console open til user closes
+        }
+    }
+}
---
+++ C#/MethodCallingSubAssign/MethodCallingSubAssign/mathOps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodCallingSubAssign
+{
+    class mathOps
+    {
+        public int doMath1(int num1)  //  Create first method
+        {
+            int answer = num1 + 1;  //  Take user number add 1
+            return answer;
+        }
+
+        public int doMath2(int num1)  //  Create second method
+        {
+            int answer = num1 * 2;  //  Take user number multiply by 2
+            return answer;
+        }
+
+        public int doMath3(int num1)  //  Create third method
+        {
+            int answer = num1 - 3;  //  Take user number subtract 3
+            return answer;
+        }
+
+    }
+}
