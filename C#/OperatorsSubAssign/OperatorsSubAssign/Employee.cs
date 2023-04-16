893a0c7b99b3945c13f87d6ed609d949abe3ce39
JRHowenstine <justin.howenstine@gmail.com>
2023-04-16 11:53:19 -0700

    Operators Submission Assignment

---
+++ C#/OperatorsSubAssign/OperatorsSubAssign/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubAssign
+{
+    public class Employee
+    {
+        public string firstName { get; set; }  //  Defining Properties
+        public string lastName { get; set; }
+        public int employeeID { get; set; }
+
+        public Employee(string firstName, string lastName, int employeeID)  //  Defining objects
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.employeeID = employeeID;
+        }
+
+        public static bool operator ==(Employee employee, Employee employee2)  //  Overloading operator to compare employee ids
+        {
+            if (employee.employeeID == employee2.employeeID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator !=(Employee employee, Employee employee2)  //  Overloaded comparison operator pair
+        {
+            if (employee.employeeID != employee2.employeeID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ C#/OperatorsSubAssign/OperatorsSubAssign/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Jon", "Smith", 5);  //  Instantiating two employee objects
+            Employee employee2 = new Employee("Jane", "Doe", 13);
+
+            if (employee == employee2)  // Comparing objects using the overloaded operator
+            {
+                Console.WriteLine("The employees have the same ID number");
+            }
+            else
+            {
+                Console.WriteLine("The employees do not have the same ID number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
