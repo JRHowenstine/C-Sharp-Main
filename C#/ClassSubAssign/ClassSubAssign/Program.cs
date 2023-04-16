95ee884c2b851da9bafe8908f13322bd5037664e
JRHowenstine <justin.howenstine@gmail.com>
2023-04-15 19:02:48 -0700

    Class Submission Assignment step 285

---
+++ C#/ClassSubAssign/ClassSubAssign/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer to be divided in half: ");  //  Ask user to input a number
+            int userNum = Convert.ToInt32(Console.ReadLine());  //  Store user number as an integer
+            divideByTwo div = new divideByTwo();  //  Instantiate the divideByTwo class
+            div.divTwo(userNum);  //  Call the class method passing in user number
+            
+            int num1;  //  define the datatype of the variable that will be the output parameter to be integer
+            divideByTwo.divTwo(out num1);  //  Call on the overloaded method with output parameters
+
+            Console.ReadLine();  //  Keep Console open til user closes
+        }
+    }
+}
---
+++ C#/ClassSubAssign/ClassSubAssign/divideByTwo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubAssign
+{
+    class divideByTwo
+    {
+        public void divTwo(int numb1)
+        {
+            int numb2 = 2;  //  Set Value 
+            int numb3 = numb1 / numb2;
+            Console.WriteLine(numb3);  //  Output the result to console
+        }
+
+        public static void divTwo(out int numb1)  //  overloaded method with output parameters and declared static
+        {
+            numb1 = 1000;  //  Set values
+            int numb2 = 2;
+            int numb3 = numb1 / numb2;
+            Console.WriteLine("If you divide {0} by {1} it equals {2}.", numb1, numb2, numb3);
+        }
+    }
+}
