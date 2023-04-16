d49f895cd2ca15a57cb13acd70d4b45abc871dc9
JRHowenstine <justin.howenstine@gmail.com>
2023-04-16 11:03:03 -0700

    Polymorphism Submission Assignment

--- C#/AbstractClassSubAssign/AbstractClassSubAssign/Employee.cs
+++ C#/AbstractClassSubAssign/AbstractClassSubAssign/Employee.cs
@@ -6,12 +6,17 @@ using System.Threading.Tasks;
 
 namespace AbstractClassSubAssign
 {
-    public class Employee : Person  //  Inherit from the Person class
+    public class Employee : Person, IQuittable  //  Inherit from the Person class and the IQuittable interface
     {
         public override void SayName()  // inherit and override the SayName() method
         {
             Console.WriteLine("Employee: ");
             base.SayName();
+        }
+
+        public void Quit()  //  Defining quit() method for this class
+        {
+            Console.WriteLine("Quit");
         }
 
     }
--- C#/AbstractClassSubAssign/AbstractClassSubAssign/Program.cs
+++ C#/AbstractClassSubAssign/AbstractClassSubAssign/Program.cs
@@ -13,6 +13,9 @@ namespace AbstractClassSubAssign
             Employee employee = new Employee() { firstName = "Sample", lastName = "Student" };  //  Instantiating an employee object
             employee.SayName();  //  Calling the SayName() method
 
+            IQuittable quit = new Employee();  //  Using Polymorphism to instantiate an object
+            quit.Quit();  //  Calling the Quit() method
+
             Console.ReadLine();
         }
     }
