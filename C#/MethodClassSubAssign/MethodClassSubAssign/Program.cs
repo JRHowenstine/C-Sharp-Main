e2c61a1487a9385172b4c96d4d1342ca8a4e0bb8
JRHowenstine <justin.howenstine@gmail.com>
2023-04-15 17:12:41 -0700

    Void Method Class Submission Assignment

---
+++ C#/MethodClassSubAssign/MethodClassSubAssign/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            voidMethod voidMethod = new voidMethod();  //  Instantiate class voidMethod
+
+            Console.WriteLine("Please enter a number: ");
+            int userNum1 = Convert.ToInt32(Console.ReadLine()); //  Get first number form user store as integer
+            Console.WriteLine("Please enter another number: ");
+            int userNum2 = Convert.ToInt32(Console.ReadLine());//  Get second number form user store as integer
+
+            voidMethod.voidMeth(userNum1,userNum2);  //  Pass both parameters into method
+            Console.ReadLine();  //  Keep console open til user closes
+        }
+    }
+}
---
+++ C#/MethodClassSubAssign/MethodClassSubAssign/voidMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubAssign
+{
+    class voidMethod
+    {
+        public static void voidMeth(int num1, int num2 )
+        {
+            int mathOp = num1 + 10;
+            Console.WriteLine("{0} is your first number plus 10, {1} is your second number.", mathOp, num2);
+            Console.ReadLine();
+        }
+    }
+}
