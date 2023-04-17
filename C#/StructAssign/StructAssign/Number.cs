b01fc402f55177916242e99cf238b503ad62329d
JRHowenstine <justin.howenstine@gmail.com>
2023-04-16 17:19:26 -0700

    Struct Assignment

---
+++ C#/StructAssign/StructAssign/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssign
+{
+    public struct Number  //  Create a struct
+    {
+        public decimal Amount { get; set; }  // Set property of struct Amount as a decimal data type
+    }
+}
---
+++ C#/StructAssign/StructAssign/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();  //  Create an object of data type Number(from the struct)
+            number.Amount = 10;  //  Set the objects amount property to a value
+            Console.WriteLine(number.Amount);  //  Print the value to console
+            Console.ReadLine();  //  Keep console open til user closes
+        }
+    }
+}
